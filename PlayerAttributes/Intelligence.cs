d4c8467fb5efae5712d5a2fe10e97bafeef9f6a5
tuitutor <62071322+tuitutor@users.noreply.github.com>
2020-03-14 11:02:26 +0300

    attr player

---
+++ PlayerAttributes/Intelligence.cs
@@ -0,0 +1,15 @@
+﻿// Intelligence Attribute that grants extra mana.
+using System;
+using UnityEngine;
+
+public class Intelligence : PlayerAttribute, IManaBonus
+{
+    // 1 point means 1% of max bonus
+    public float manaBonusPercentPerPoint = 0.01f;
+
+    public int GetManaBonus(int baseMana)
+    {
+        return Convert.ToInt32(baseMana * (value * manaBonusPercentPerPoint));
+    }
+    public int GetManaRecoveryBonus() { return 0; }
+}
---
+++ PlayerAttributes/Strength.cs
@@ -0,0 +1,15 @@
+﻿// Strength Attribute that grants extra health.
+using System;
+using UnityEngine;
+
+public class Strength : PlayerAttribute, IHealthBonus
+{
+    // 1 point means 1% of max bonus
+    public float healthBonusPercentPerPoint = 0.01f;
+
+    public int GetHealthBonus(int baseHealth)
+    {
+        return Convert.ToInt32(baseHealth * (value * healthBonusPercentPerPoint));
+    }
+    public int GetHealthRecoveryBonus() { return 0; }
+}
